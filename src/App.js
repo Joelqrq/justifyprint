44f22aeecd7b6cd74c123c6d91d0f07039a97efb
JoelQ <joelquey@hotmail.com>
2022-08-29 23:31:23 +0800

    Added TailwindCSS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { Header } from './common/Header';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HelmetProvider>
+      <div className="App">
+        <Helmet>
+          <title>Justify Print</title>
+          <meta name="title" content="Justify Print — Cheapest and most affordable printing solutions" />
+          <meta name="description" content="Justify Print brings you the cheapest and most affordable printing solutions in Singapore." />
+          <link rel="canonical" href="https://justifyprint.com" />
+
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://justifyprint.com/" />
+          <meta property="og:title" content="Justify Print — Cheapest and most affordable printing solutions" />
+          <meta property="og:description" content="Justify Print brings you the cheapest and most affordable printing solutions in Singapore." />
+          {/* <meta property="og:image" content="" /> */}
+
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:url" content="https://justifyprint.com/" />
+          <meta property="twitter:title" content="Justify Print — Cheapest and most affordable printing solutions" />
+          <meta property="twitter:description" content="Justify Print brings you the cheapest and most affordable printing solutions in Singapore." />
+          {/* <meta property="twitter:image" content="" /> */}
+        </Helmet>
+        <Header>
+
+        </Header>
+      </div>
+    </HelmetProvider>
   );
 }
 
