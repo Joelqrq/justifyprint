3f1e45df7b636ab737c7fcde6c91ac7113a7fdb2
JoelQ <joelquey@hotmail.com>
2022-10-15 01:17:54 +0800

    Fixed empty href warning

--- src/pages/MoneyPacket.js
+++ src/pages/MoneyPacket.js
@@ -18,165 +18,165 @@ import mp13 from '../assets/images/MP13.jpg';
 const MoneyPacket = () => {
   return (
     <div className="container px-6 py-10 mx-auto space-y-6">
-      <div class="gallery">
-        <div class="gallery__column">
-          <a href="" class="gallery__link">
-            <figure class="gallery__thumb">
+      <div className="gallery">
+        <div className="gallery__column">
+          <a className="gallery__link cursor-pointer">
+            <figure className="gallery__thumb">
               <img
                 src={mpmodel}
                 alt="Portrait by Jessica Felicio"
-                class="gallery__image"
+                className="gallery__image"
               />
-              <figcaption class="gallery__caption"></figcaption>
+              <figcaption className="gallery__caption"></figcaption>
             </figure>
           </a>
 
-          <a href="" class="gallery__link">
-            <figure class="gallery__thumb">
+          <a className="gallery__link cursor-pointer">
+            <figure className="gallery__thumb">
               <img
                 src={mp1}
                 alt="Portrait by Oladimeji Odunsi"
-                class="gallery__image"
+                className="gallery__image"
               />
-              <figcaption class="gallery__caption"></figcaption>
+              <figcaption className="gallery__caption"></figcaption>
             </figure>
           </a>
 
-          <a href="" class="gallery__link">
-            <figure class="gallery__thumb">
+          <a className="gallery__link cursor-pointer">
+            <figure className="gallery__thumb">
               <img
                 src={mp2}
                 alt="Portrait by Alex Perez"
-                class="gallery__image"
+                className="gallery__image"
               />
-              <figcaption class="gallery__caption"></figcaption>
+              <figcaption className="gallery__caption"></figcaption>
             </figure>
           </a>
         </div>
 
-        <div class="gallery__column">
-          <a href="" class="gallery__link">
-            <figure class="gallery__thumb">
+        <div className="gallery__column">
+          <a className="gallery__link cursor-pointer">
+            <figure className="gallery__thumb">
               <img
                 src={mp3}
                 alt="Portrait by Noah Buscher"
-                class="gallery__image"
+                className="gallery__image"
               />
-              <figcaption class="gallery__caption"></figcaption>
+              <figcaption className="gallery__caption"></figcaption>
             </figure>
           </a>
 
-          <a href="" class="gallery__link">
-            <figure class="gallery__thumb">
+          <a className="gallery__link cursor-pointer">
+            <figure className="gallery__thumb">
               <img
                 src={mp4}
                 alt="Portrait by Ivana Cajina"
-                class="gallery__image"
+                className="gallery__image"
               />
-              <figcaption class="gallery__caption"></figcaption>
+              <figcaption className="gallery__caption"></figcaption>
             </figure>
           </a>
 
-          <a href="" class="gallery__link">
-            <figure class="gallery__thumb">
+          <a className="gallery__link cursor-pointer">
+            <figure className="gallery__thumb">
               <img
                 src={mp5}
                 alt="Portrait by Sam Burriss"
-                class="gallery__image"
+                className="gallery__image"
               />
-              <figcaption class="gallery__caption"></figcaption>
+              <figcaption className="gallery__caption"></figcaption>
             </figure>
           </a>
         </div>
 
-        <div class="gallery__column">
-          <a href="" class="gallery__link">
-            <figure class="gallery__thumb">
+        <div className="gallery__column">
+          <a className="gallery__link cursor-pointer">
+            <figure className="gallery__thumb">
               <img
                 src={mp6}
                 alt="Portrait by Mari Lezhava"
-                class="gallery__image"
+                className="gallery__image"
               />
-              <figcaption class="gallery__caption"></figcaption>
+              <figcaption className="gallery__caption"></figcaption>
             </figure>
           </a>
 
-          <a href="" class="gallery__link">
-            <figure class="gallery__thumb">
+          <a className="gallery__link cursor-pointer">
+            <figure className="gallery__thumb">
               <img
                 src={mp7}
                 alt="Portrait by Ethan Haddox"
-                class="gallery__image"
+                className="gallery__image"
               />
-              <figcaption class="gallery__caption"></figcaption>
+              <figcaption className="gallery__caption"></figcaption>
             </figure>
           </a>
 
-          <a href="" class="gallery__link">
-            <figure class="gallery__thumb">
+          <a className="gallery__link cursor-pointer">
+            <figure className="gallery__thumb">
               <img
                 src={mp8}
                 alt="Portrait by Amir Geshani"
-                class="gallery__image"
+                className="gallery__image"
               />
-              <figcaption class="gallery__caption"></figcaption>
+              <figcaption className="gallery__caption"></figcaption>
             </figure>
           </a>
         </div>
 
-        <div class="gallery__column">
-          <a href="" class="gallery__link">
-            <figure class="gallery__thumb">
+        <div className="gallery__column">
+          <a className="gallery__link cursor-pointer">
+            <figure className="gallery__thumb">
               <img
                 src={mp9}
                 alt="Portrait by Guilian Fremaux"
-                class="gallery__image"
+                className="gallery__image"
               />
-              <figcaption class="gallery__caption"></figcaption>
+              <figcaption className="gallery__caption"></figcaption>
             </figure>
           </a>
 
-          <a href="" class="gallery__link">
-            <figure class="gallery__thumb">
+          <a className="gallery__link cursor-pointer">
+            <figure className="gallery__thumb">
               <img
                 src={mp10}
                 alt="Portrait by Jasmin Chew"
-                class="gallery__image"
+                className="gallery__image"
               />
-              <figcaption class="gallery__caption"></figcaption>
+              <figcaption className="gallery__caption"></figcaption>
             </figure>
           </a>
 
-          <a href="" class="gallery__link">
-            <figure class="gallery__thumb">
+          <a className="gallery__link cursor-pointer">
+            <figure className="gallery__thumb">
               <img
                 src={mp11}
                 alt="Portrait by Dima DallAcqua"
-                class="gallery__image"
+                className="gallery__image"
               />
-              <figcaption class="gallery__caption"></figcaption>
+              <figcaption className="gallery__caption"></figcaption>
             </figure>
           </a>
 
-          <a href="" class="gallery__link">
-            <figure class="gallery__thumb">
+          <a className="gallery__link cursor-pointer">
+            <figure className="gallery__thumb">
               <img
                 src={mp12}
                 alt="Portrait by Dima DallAcqua"
-                class="gallery__image"
+                className="gallery__image"
               />
-              <figcaption class="gallery__caption"></figcaption>
+              <figcaption className="gallery__caption"></figcaption>
             </figure>
           </a>
 
-          <a href="" class="gallery__link">
-            <figure class="gallery__thumb">
+          <a className="gallery__link cursor-pointer">
+            <figure className="gallery__thumb">
               <img
                 src={mp13}
                 alt="Portrait by Dima DallAcqua"
-                class="gallery__image"
+                className="gallery__image"
               />
-              <figcaption class="gallery__caption"></figcaption>
+              <figcaption className="gallery__caption"></figcaption>
             </figure>
           </a>
         </div>
