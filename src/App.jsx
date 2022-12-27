9a7bf89c6720179b909921eef8a421cca2523b7f
JoelQ <joelquey@hotmail.com>
2022-12-27 15:03:32 +0800

    Moved metatags to index.html

--- src/App.jsx
+++ src/App.jsx
@@ -14,39 +14,6 @@ function App() {
       <div className="App">
         <Helmet>
           <title>Justify Print: Quality and affordable printing solutions</title>
-          <meta
-            name="title"
-            content="Justify Print: Quality and affordable printing solutions"
-          />
-          <meta
-            name="description"
-            content="Justify Print brings you the quality and affordable printing solutions in Singapore."
-          />
-          <link rel="canonical" href="https://justifyprint.com" />
-
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content="https://justifyprint.com/" />
-          <meta
-            property="og:title"
-            content="Justify Print: Quality and affordable printing solutions"
-          />
-          <meta
-            property="og:description"
-            content="Justify Print brings you the quality and affordable printing solutions in Singapore."
-          />
-          <meta property="og:image" content="https://justifyprint.com/favicon.png" />
-
-          <meta property="twitter:card" content="summary_large_image" />
-          <meta property="twitter:url" content="https://justifyprint.com/" />
-          <meta
-            property="twitter:title"
-            content="Justify Print: Quality and affordable printing solutions"
-          />
-          <meta
-            property="twitter:description"
-            content="Justify Print brings you the quality and affordable printing solutions in Singapore."
-          />
-          <meta property="twitter:image" content="https://justifyprint.com/favicon.png" />
         </Helmet>
         <BrowserRouter>
           <Navbar></Navbar>
