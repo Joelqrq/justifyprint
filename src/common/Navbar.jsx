b9cc0e7633b9040c9a0e513e00d6c49aeae5e899
JoelQ <joelquey@hotmail.com>
2023-06-04 15:41:07 +0800

    fix: fixed navlinks in mobile mode... - update browserlist

--- src/common/Navbar.jsx
+++ src/common/Navbar.jsx
@@ -112,7 +112,7 @@ export const Navbar = () => {
                     <li>
                       <NavLink
                         onClick={setOpenState}
-                        to="moneypacket"
+                        to="money-packet"
                         className="block p-3 rounded-lg border border-transparent hover:border-zinc-900/10 dark:hover:border-zinc-100/10 hover:bg-zinc-100/20 dark:hover:bg-zinc-800/20 text-center transition-colors duration-[50ms]"
                       >
                         <p className="capitalize text-zinc-800 dark:text-zinc-100 font-semibold">
@@ -126,7 +126,7 @@ export const Navbar = () => {
                     <li>
                       <NavLink
                         onClick={setOpenState}
-                        to="custombox"
+                        to="custom-box"
                         className="block p-3 rounded-lg border border-transparent hover:border-zinc-900/10 dark:hover:border-zinc-100/10 hover:bg-zinc-100/20 dark:hover:bg-zinc-800/20 text-center transition-colors duration-[50ms]"
                       >
                         <p className="capitalize text-zinc-800 dark:text-zinc-100 font-semibold">
