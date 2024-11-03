630ee2cb48ce2eeca3bdcaf7373b2ff5e3223e60
joelq <joel.queyruiquan@temus.com>
2024-11-03 20:45:43 +0800

    feat: add logo infinite slider

--- src/landing/ClientTestimony.jsx
+++ src/landing/ClientTestimony.jsx
@@ -1,33 +1,188 @@
 import React from 'react';
 
 export const ClientTestimony = () => {
+  
   return (
     <div>
-      <div className="max-w-screen-xl flex flex-col px-6 dark:px-16 dark:lg:px-6 py-12 mx-auto items-center">
-        <span className="block mb-3 dark:mb-6 text-xl font-semibold text-zinc-800 dark:text-zinc-100 text-center md:text-start">
-          Companies to individuals we worked with
+      <div className="max-w-screen-xl flex flex-col px-6 py-12 mx-auto items-center">
+        <span className="block mb-6 text-xl font-semibold text-zinc-800 dark:text-zinc-100 text-center md:text-start">
+          Companies and individuals with whom we have collaborated
         </span>
-        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 auto-cols-min gap-x-4 gap-y-1 dark:gap-y-4 place-items-center">
-          <div className="h-full w-full flex px-2 lg:px-0 dark:lg:px-2 justify-center items-center dark:bg-zinc-50 dark:rounded">
-            <img className="h-28 pt-1 object-contain mix-blend-multiply" src="/images/clients-logo/bnc.webp" title="bnc logo" />
+        <div className="group w-screen flex overflow-hidden gap-x-6 select-none [mask-image:linear-gradient(to_right,hsla(0,0%,0%,0),hsla(0,0%,0%,1)_20%,hsla(0,0%,0%,1)_80%,hsla(0,0%,0%,0))]">
+          <div className="min-w-full flex flex-shrink-0 justify-around gap-x-6 animate-infinite-slider group-hover:[animation-play-state:paused]">
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-2 object-contain" src="/images/clients-logo/bnc.webp" title="bnc logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-5 object-contain" src="/images/clients-logo/oishi.svg" title="oishi logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-6 object-contain" src="/images/clients-logo/ntu.webp" title="ntu logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 pb-4 object-contain" src="/images/clients-logo/renci.png" title="renci logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-5 object-contain" src="/images/clients-logo/nus.png" title="nus logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-3 object-contain" src="/images/clients-logo/mindef.png" title="mindef logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-2 object-contain" src="/images/clients-logo/aia.png" title="aia logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-2 object-contain" src="/images/clients-logo/da-paolo.png" title="da palo logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-2 object-contain" src="/images/clients-logo/defence-collective.png" title="defence collective logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-2 object-contain" src="/images/clients-logo/fei-yue.png" title="fei yue logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-2 object-contain" src="/images/clients-logo/fujiware-tofushop.png" title="fujiware tofushop logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-2 object-contain" src="/images/clients-logo/gofit.png" title="gofit logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-2 object-contain" src="/images/clients-logo/gown-connoisseur.png" title="the gown connoisseur logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-2 object-contain" src="/images/clients-logo/great-eastern.png" title="great eastern logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-2 object-contain" src="/images/clients-logo/higer.png" title="higer logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-2 object-contain" src="/images/clients-logo/kindness-movement.png" title="kindness movement logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-2 object-contain" src="/images/clients-logo/kuehne-nagel.png" title="kuehne nagel logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-2 object-contain" src="/images/clients-logo/navy.png" title="navy logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-2 object-contain" src="/images/clients-logo/ppis.png" title="ppis logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-2 object-contain" src="/images/clients-logo/prudential.png" title="prudential logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-2 object-contain" src="/images/clients-logo/red-cross.png" title="red cross logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-2 object-contain" src="/images/clients-logo/sim.png" title="sim logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-2 object-contain" src="/images/clients-logo/smrt.png" title="smrt logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-2 object-contain" src="/images/clients-logo/snatchjobs.png" title="snatchjobs logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-2 object-contain" src="/images/clients-logo/sree-narayana.png" title="sree narayana logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-2 object-contain" src="/images/clients-logo/st-andrew.png" title="st andrew logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-2 object-contain" src="/images/clients-logo/sumitomo-bakelite.png" title="sumitomo bakelite logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-2 object-contain" src="/images/clients-logo/tanjong-pagar-town-council.png" title="tanjong pagar town council logo" />
+            </div>
           </div>
-          <div className="h-full w-full flex px-4 lg:px-4 -mt-6 dark:mt-0 -mb-3 dark:mb-0 lg:my-0 justify-center items-center dark:bg-zinc-50 dark:rounded">
-            <img className="h-28 py-6 object-contain" src="/images/clients-logo/oishi.svg" title="oishi logo" />
+          <div className="min-w-full flex flex-shrink-0 justify-around gap-x-6 animate-infinite-slider group-hover:[animation-play-state:paused]">
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-2 object-contain" src="/images/clients-logo/bnc.webp" title="bnc logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-5 object-contain" src="/images/clients-logo/oishi.svg" title="oishi logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-6 object-contain" src="/images/clients-logo/ntu.webp" title="ntu logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 pb-4 object-contain" src="/images/clients-logo/renci.png" title="renci logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-5 object-contain" src="/images/clients-logo/nus.png" title="nus logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-3 object-contain" src="/images/clients-logo/mindef.png" title="mindef logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-2 object-contain" src="/images/clients-logo/aia.png" title="aia logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-2 object-contain" src="/images/clients-logo/da-paolo.png" title="da palo logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-2 object-contain" src="/images/clients-logo/defence-collective.png" title="defence collective logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-2 object-contain" src="/images/clients-logo/fei-yue.png" title="fei yue logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-2 object-contain" src="/images/clients-logo/fujiware-tofushop.png" title="fujiware tofushop logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-2 object-contain" src="/images/clients-logo/gofit.png" title="gofit logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-2 object-contain" src="/images/clients-logo/gown-connoisseur.png" title="the gown connoisseur logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-2 object-contain" src="/images/clients-logo/great-eastern.png" title="great eastern logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-2 object-contain" src="/images/clients-logo/higer.png" title="higer logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-2 object-contain" src="/images/clients-logo/kindness-movement.png" title="kindness movement logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-2 object-contain" src="/images/clients-logo/kuehne-nagel.png" title="kuehne nagel logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-2 object-contain" src="/images/clients-logo/navy.png" title="navy logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-2 object-contain" src="/images/clients-logo/ppis.png" title="ppis logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-2 object-contain" src="/images/clients-logo/prudential.png" title="prudential logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-2 object-contain" src="/images/clients-logo/red-cross.png" title="red cross logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-2 object-contain" src="/images/clients-logo/sim.png" title="sim logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-2 object-contain" src="/images/clients-logo/smrt.png" title="smrt logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-2 object-contain" src="/images/clients-logo/snatchjobs.png" title="snatchjobs logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-2 object-contain" src="/images/clients-logo/sree-narayana.png" title="sree narayana logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-2 object-contain" src="/images/clients-logo/st-andrew.png" title="st andrew logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-2 object-contain" src="/images/clients-logo/sumitomo-bakelite.png" title="sumitomo bakelite logo" />
+            </div>
+            <div className="h-36 lg:h-28 min-w-[10rem] px-2 flex justify-center items-center bg-white rounded-sm">
+              <img className="h-36 lg:h-28 w-36 py-2 object-contain" src="/images/clients-logo/tanjong-pagar-town-council.png" title="tanjong pagar town council logo" />
+            </div>
           </div>
-          <div className="h-full w-full flex px-3 lg:px-4 dark:lg:px-5 justify-center items-center dark:bg-zinc-50 dark:rounded">
-            <img className="h-28 py-5 object-contain" src="/images/clients-logo/ntu.webp" title="ntu logo" />
-          </div>
-          <div className="h-full w-full flex px-3 lg:px-4 justify-center items-center dark:bg-zinc-50 dark:rounded">
-            <img className="h-28 dark:pt-2 dark:lg:pt-0 pb-4 dark:pb-2 dark:lg:pb-3  object-contain mix-blend-multiply" src="/images/clients-logo/renci.png" title="renci logo" />
-          </div>
-          <div className="h-full w-full flex px-3 lg:px-4 dark:lg:px-8 justify-center items-center dark:bg-zinc-50 dark:rounded">
-            <img className="h-28 py-5 object-contain" src="/images/clients-logo/nus.png" title="nus logo" />
-          </div>
-          <div className="h-full w-full flex px-3 lg:px-4 dark:lg:px-8 justify-center items-center dark:bg-zinc-50 dark:rounded">
-            <img className="h-28 py-5 object-contain mix-blend-multiply" src="/images/clients-logo/mindef.png" title="mindef logo" />
-          </div>
-        </div>
         </div>
       </div>
+    </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./src/**/*.jsx"],
   theme: {
     extend: {
+      animation: {
+        "infinite-slider": "60000ms slider infinite linear"
+      },
+      keyframes: {
+        slider: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" }
+        }
+      },
       fontFamily: {
         sans: ["Inter var"],
       },
